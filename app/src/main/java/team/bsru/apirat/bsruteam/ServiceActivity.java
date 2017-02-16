0e13e65b50bf0b6864906da8330e57cc9f209089
khongying <kongying_99_kunket@hotmail.com>
2017-02-16 15:21:16 +0700

    Google Map V2 by ice

--- app/src/main/java/team/bsru/apirat/bsruteam/ServiceActivity.java
+++ app/src/main/java/team/bsru/apirat/bsruteam/ServiceActivity.java
@@ -2,6 +2,8 @@ package team.bsru.apirat.bsruteam;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,11 +17,25 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     //Explicit
     private GoogleMap mMap;
     private double userLatADouble = 13.733064, userLngADouble = 100.489400;
+    private TextView textView;
+    private Button button;
+    private String[] loginString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
+
+        // Bind Widget
+        textView = (TextView) findViewById(R.id.textView2);
+        button = (Button) findViewById(R.id.button2);
+
+        // Receive Value for MainActActivity
+        loginString = getIntent().getStringArrayExtra("Login");
+
+        //Show Text
+        textView.setText(loginString[1]);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
